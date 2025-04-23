4614c43dceac795dd7d27c6dd33ce54e29c52494
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-23 17:55:21 +0700

    menambahkan body dengan eleveted button dan navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Home Page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Selamat datang di Home Page!'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OrderPage()),
+                );
+              },
+              child: Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
