c81da0a7aeb93fba1cfdc162b8d5faeb5daafd31
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-23 18:14:29 +0700

    membuat variable dan constructor

--- lib/pages/detail_order_page.dart
+++ lib/pages/detail_order_page.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
