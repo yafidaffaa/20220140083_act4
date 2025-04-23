0dd0054b181582398e4a2f5cc42a1cfb4c545f8c
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-23 18:21:26 +0700

    memanggil variabel inputan user dan elevated button dengan navigation

--- lib/pages/detail_order_page.dart
+++ lib/pages/detail_order_page.dart
@@ -18,6 +18,33 @@ class DetailOrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
