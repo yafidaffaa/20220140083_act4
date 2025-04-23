b6d99e9da9c2d9bdbdb702a9765401b2633d64cc
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-23 18:01:48 +0700

    membuat function claculatetotalprice dan setstate untuk memperbarui totalharga

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -14,6 +14,15 @@ class _OrderPageState extends State<OrderPage> {
   final TextEditingController jumlahMinumanController = TextEditingController();
   int totalHarga = 0;
 
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
